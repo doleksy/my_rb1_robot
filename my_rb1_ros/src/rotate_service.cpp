c7c3082c4e26194877071d60e812b390c9e21024
Dave Oleksy <dave@doleksy.com>
2024-04-02 21:13:10 +0000

    Moved server to a class; sped up rotation

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -7,87 +7,106 @@
 
 #include "my_rb1_ros/Rotate.h"
 
-namespace
+class RotateRobotServer
 {
-    double odomYaw{};
-}   // unnamed
-
-bool server_callback(my_rb1_ros::RotateRequest  &req,
-                     my_rb1_ros::RotateResponse &res)
-{
-    const double rotationRadians{angles::from_degrees(req.degrees)};
-    ROS_INFO("Received: %d degrees: %f radians", req.degrees, rotationRadians);
-
-    ros::NodeHandle nh;
-    ros::Publisher rotate = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
-
-    double destRadians{odomYaw + rotationRadians};
-    if (destRadians > TF2SIMD_PI)
+public:
+    RotateRobotServer()
+        : nh_{}
+        , server_(nh_.advertiseService("/rotate_robot", &RotateRobotServer::server_callback, this))
+        , odom_(nh_.subscribe("/odom", 1000, &RotateRobotServer::odom_callback, this))
+        , rotate_(nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000))
     {
-        destRadians -= TF2SIMD_2_PI;
-    }
-    else if (destRadians < -TF2SIMD_PI)
-    {
-        destRadians += TF2SIMD_2_PI;
-    }
-    ROS_INFO("odomYaw: %f (%f degrees); dest: %f radians (%f degrees)", odomYaw, angles::to_degrees(odomYaw), destRadians, angles::to_degrees(destRadians));
-
-    geometry_msgs::Twist rotateMsg;
-    if (req.degrees > 0)
-    {
-        rotateMsg.angular.z = 0.1;
-    }
-    else
-    {
-        rotateMsg.angular.z = -0.1;
+        ROS_INFO("Service Ready");
     }
 
-    ros::Rate rotateRate(20);
-    constexpr double epsilon{0.02};
 
-    while (std::abs(destRadians - odomYaw) > epsilon)
+private:
+    ros::NodeHandle nh_;
+    ros::ServiceServer server_;
+    ros::Subscriber odom_;
+    ros::Publisher rotate_;
+    
+    ros::Rate rotateRate_{20};
+
+    double odomYaw_{};
+
+
+private:
+    bool server_callback(my_rb1_ros::RotateRequest  &req,
+                         my_rb1_ros::RotateResponse &res)
     {
-        //ROS_INFO("dest: %f yaw: %f diff: %f", destRadians, odomYaw, std::abs(destRadians - odomYaw));
-        rotate.publish(rotateMsg);
-        ros::spinOnce();
-        rotateRate.sleep();
+        const double rotationRadians{angles::from_degrees(req.degrees)};
+        ROS_INFO("Service Requested: %d degrees: %f radians", req.degrees, rotationRadians);
+
+        double destRadians{odomYaw_ + rotationRadians};
+        if (destRadians > TF2SIMD_PI)
+        {
+            destRadians -= TF2SIMD_2_PI;
+        }
+        else if (destRadians < -TF2SIMD_PI)
+        {
+            destRadians += TF2SIMD_2_PI;
+        }
+        ROS_DEBUG("odomYaw_: %f (%f degrees); dest: %f radians (%f degrees)", odomYaw_, angles::to_degrees(odomYaw_), destRadians, angles::to_degrees(destRadians));
+
+        geometry_msgs::Twist rotateMsg{};
+        if (req.degrees > 0)
+        {
+            rotateMsg.angular.z = 0.65;
+        }
+        else
+        {
+            rotateMsg.angular.z = -0.65;
+        }
+
+        constexpr double epsilon{0.015};
+
+        while (std::abs(destRadians - odomYaw_) > epsilon)
+        {
+            ROS_DEBUG("dest: %f yaw: %f diff: %f", destRadians, odomYaw_, std::abs(destRadians - odomYaw_));
+            rotate_.publish(rotateMsg);
+            ros::spinOnce();
+            rotateRate_.sleep();
+        }
+
+        rotateMsg.angular.z = 0;
+        for (int i = 0; i < 4; ++i)
+        {
+            rotate_.publish(rotateMsg);
+            rotateRate_.sleep();
+        }
+
+        ROS_INFO("Service Completed");
+
+        res.result = "Rotation Successful";
+        return true;
     }
 
-    rotateMsg.angular.z = 0;
-    for (int i = 0; i < 4; ++i)
+    void odom_callback(const nav_msgs::OdometryConstPtr &odom)
     {
-        rotate.publish(rotateMsg);
-        rotateRate.sleep();
+        geometry_msgs::Quaternion orient = odom->pose.pose.orientation;
+        ROS_DEBUG("Odom orientation: x=%f y=%f z=%f w=%f", orient.x, orient.y, orient.z, orient.w);
+
+        tf2::Quaternion quat_tf;
+        tf2::fromMsg(orient, quat_tf);
+
+        tf2::Matrix3x3 m(quat_tf);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+        ROS_DEBUG("Odom roll=%f pitch=%f yaw=%f", roll, pitch, yaw);
+
+        odomYaw_ = yaw;
     }
 
-    res.result = "Rotation Successful";
-    return true;
-}
+};  // class RotateRobotServer
 
-void odom_callback(const nav_msgs::OdometryConstPtr &odom)
-{
-    geometry_msgs::Quaternion orient = odom->pose.pose.orientation;
-    //ROS_INFO("Odom orientation: x=%f y=%f z=%f w=%f", orient.x, orient.y, orient.z, orient.w);
-
-    tf2::Quaternion quat_tf;
-    tf2::fromMsg(orient, quat_tf);
-
-    tf2::Matrix3x3 m(quat_tf);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-    //ROS_INFO("Odom roll=%f pitch=%f yaw=%f", roll, pitch, yaw);
-
-    odomYaw = yaw;
-}
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "rotate_service");
-    ros::NodeHandle nh;
 
-    ros::ServiceServer my_srv = nh.advertiseService("/rotate_robot", server_callback);
+    RotateRobotServer robot_server;    
 
-    ros::Subscriber odom = nh.subscribe("/odom", 1000, odom_callback);
     ros::spin();
 
     return 0;

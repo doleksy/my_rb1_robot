ae1355efb9f8e068859ec5416253ce6a7afd3411
Dave Oleksy <dave@doleksy.com>
2024-04-04 03:48:39 +0000

    Added full circle rotation for degrees >= 360; still need to test

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
@@ -6,6 +7,7 @@
 #include <cmath>
 
 #include "my_rb1_ros/Rotate.h"
+#include "tf2/LinearMath/Scalar.h"
 
 class RotateRobotServer
 {
@@ -28,17 +30,67 @@ private:
     
     ros::Rate rotateRate_{20};
 
+    int32_t degrees_{};
     double odomYaw_{};
+
+    static constexpr const double rotationAmount_{ 0.65 };
+    static constexpr const double epsilon_{ 0.015 };
 
 
 private:
     bool server_callback(my_rb1_ros::RotateRequest  &req,
                          my_rb1_ros::RotateResponse &res)
     {
-        const double rotationRadians{angles::from_degrees(req.degrees)};
-        ROS_INFO("Service Requested: %d degrees: %f radians", req.degrees, rotationRadians);
+        degrees_ = req.degrees;
+        const double rotationRadians{angles::from_degrees(degrees_)};
+        ROS_INFO("Service Requested: %d degrees: %f radians", degrees_, rotationRadians);
 
-        double destRadians{odomYaw_ + rotationRadians};
+        auto remainingRotationRadians = rotate_full_circles(rotationRadians);
+
+        rotate_sub_360(remainingRotationRadians);
+
+        stop_rotation();
+
+        ROS_INFO("Service Completed");
+
+        res.result = "Rotation Successful";
+        return true;
+    }
+
+    double rotate_full_circles(double rotationRadians)
+    {
+        if (rotationRadians < TF2SIMD_PI) return rotationRadians;
+
+        ROS_INFO(">= 360: odomYaw_: %f (%f degrees); dest: %f radians (%f degrees)", odomYaw_, angles::to_degrees(odomYaw_), rotationRadians, angles::to_degrees(rotationRadians));
+
+        geometry_msgs::Twist rotateMsg{};
+        if (degrees_ > 0)
+        {
+            rotateMsg.angular.z = rotationAmount_;
+        }
+        else
+        {
+            rotateMsg.angular.z = -rotationAmount_;
+        }
+
+        double startingOdomYaw{ odomYaw_ };
+        do {
+            ROS_DEBUG(">= 360: dest: %f yaw: %f diff: %f", startingOdomYaw, odomYaw_, std::abs(startingOdomYaw - odomYaw_));
+            rotate_.publish(rotateMsg);
+            ros::spinOnce();
+            rotateRate_.sleep();
+        } while (std::abs(startingOdomYaw - odomYaw_) > epsilon_);
+
+        rotationRadians -= TF2SIMD_2_PI;
+
+        return rotationRadians;
+    }
+
+    void rotate_sub_360(double remainingRotationRadians)
+    {
+        assert(remainingRotationRadians < TF2SIMD_2_PI);
+
+        double destRadians{odomYaw_ + remainingRotationRadians};
         if (destRadians > TF2SIMD_PI)
         {
             destRadians -= TF2SIMD_2_PI;
@@ -47,45 +99,41 @@ private:
         {
             destRadians += TF2SIMD_2_PI;
         }
-        ROS_DEBUG("odomYaw_: %f (%f degrees); dest: %f radians (%f degrees)", odomYaw_, angles::to_degrees(odomYaw_), destRadians, angles::to_degrees(destRadians));
+        ROS_INFO("< 360: odomYaw_: %f (%f degrees); dest: %f radians (%f degrees)", odomYaw_, angles::to_degrees(odomYaw_), destRadians, angles::to_degrees(destRadians));
 
         geometry_msgs::Twist rotateMsg{};
-        if (req.degrees > 0)
+        if (degrees_ > 0)
         {
-            rotateMsg.angular.z = 0.65;
+            rotateMsg.angular.z = rotationAmount_;
         }
         else
         {
-            rotateMsg.angular.z = -0.65;
+            rotateMsg.angular.z = -rotationAmount_;
         }
 
-        constexpr double epsilon{0.015};
-
-        while (std::abs(destRadians - odomYaw_) > epsilon)
+        while (std::abs(destRadians - odomYaw_) > epsilon_)
         {
-            ROS_DEBUG("dest: %f yaw: %f diff: %f", destRadians, odomYaw_, std::abs(destRadians - odomYaw_));
+            //ROS_DEBUG("dest: %f yaw: %f diff: %f", destRadians, odomYaw_, std::abs(destRadians - odomYaw_));
             rotate_.publish(rotateMsg);
             ros::spinOnce();
             rotateRate_.sleep();
         }
+    }
 
-        rotateMsg.angular.z = 0;
+    void stop_rotation(void)
+    {
+        geometry_msgs::Twist rotateMsg{};
         for (int i = 0; i < 4; ++i)
         {
             rotate_.publish(rotateMsg);
             rotateRate_.sleep();
         }
-
-        ROS_INFO("Service Completed");
-
-        res.result = "Rotation Successful";
-        return true;
     }
-
+    
     void odom_callback(const nav_msgs::OdometryConstPtr &odom)
     {
         geometry_msgs::Quaternion orient = odom->pose.pose.orientation;
-        ROS_DEBUG("Odom orientation: x=%f y=%f z=%f w=%f", orient.x, orient.y, orient.z, orient.w);
+        //ROS_DEBUG("Odom orientation: x=%f y=%f z=%f w=%f", orient.x, orient.y, orient.z, orient.w);
 
         tf2::Quaternion quat_tf;
         tf2::fromMsg(orient, quat_tf);
@@ -93,7 +141,7 @@ private:
         tf2::Matrix3x3 m(quat_tf);
         double roll, pitch, yaw;
         m.getRPY(roll, pitch, yaw);
-        ROS_DEBUG("Odom roll=%f pitch=%f yaw=%f", roll, pitch, yaw);
+        //ROS_DEBUG("Odom roll=%f pitch=%f yaw=%f", roll, pitch, yaw);
 
         odomYaw_ = yaw;
     }
